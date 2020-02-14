966f5e900eb6def76f49e833888374e53b53c6e8
Keith Cirkel <keithamus@users.noreply.github.com>
2020-02-14 15:17:39 +0000

    chore: update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const walk = require('acorn-walk')
 const MagicString = require('magic-string')
-const {createFilter} = require('rollup-pluginutils')
+const {createFilter} = require('@rollup/pluginutils')
 const {relative} = require('path')
 module.exports = function transfromInvariantCalls({include, exclude, dir = ''} = {}) {
   const filter = createFilter(include, exclude)
